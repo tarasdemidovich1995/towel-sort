c36704630e96e5ddc8af5c91bc9ca653bf4b85f9
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-20 11:17:28 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (arguments.length == 0 || matrix.length == 0) {
+        return [];
+    }
+    let direction = 'fromLeftToRight';
+    let newArr = [];
+    for (let y = 0; y < matrix.length; y++) {
+        if (direction == 'fromLeftToRight') {
+            for (let x = 0; x < matrix[y].length; x++) {
+                newArr.push(matrix[y][x]);
+            }
+            direction = 'fromRightToLeft';
+        } else if (direction == 'fromRightToLeft') {
+            for (let x = matrix[y].length - 1; x >= 0; x--) {
+                newArr.push(matrix[y][x])
+            }
+            direction = 'fromLeftToRight';
+        }
+    }
+    return newArr;
 }
